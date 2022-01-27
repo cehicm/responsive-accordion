af111bd2c75e637ecb14bc6380afd735df72826d
Marija Cehic <marijac@stuntcoders.com>
2022-01-27 09:50:45 +0100

    Fix accordion to show one element at a time

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,7 +1,19 @@
-(function ($) {
-	$('.list-item').on('click', function() {
-	$(this).children('.question-answer').slideToggle('slow');
-    $(this).children('.question-title').toggleClass('text-dark--bold ');
-	$(this).children('svg').toggleClass('rotate-180');
-   });
+(function () {
+  $(".list-item").on("click", function () {
+    $(this).toggleClass("open");
+
+    //Collapse active element when a new one is opened
+    if ($(this).hasClass("open")) {
+      $(".list-item").not(this).removeClass("open");
+    }
+    //Revert arrow to original position when oanother element is opened
+    const questionButton = $(this).children("svg");
+    $("svg").not(questionButton).removeClass("rotate-180");
+    questionButton.toggleClass("rotate-180");
+
+    //Revert text style when not active
+    const questionTitle = $(this).children(".question-title");
+    $(".question-title").not(questionTitle).removeClass("text-dark--bold");
+    questionTitle.toggleClass("text-dark--bold");
+  });
 })(jQuery);
